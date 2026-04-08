5e5900f962bc66d32a36a5953fc7e29d0e2f78e8
Akash Biswal <akashbiswaliit7@gmail.com>
2026-04-08 12:04:00 +0530

    deploy: changes in db

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def create_app():
 
     with app.app_context():
         print("Engine URL:", db.engine.url)
+        print("creating tables...")
+        db.create_all()
 
     # Register Blueprints
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
@@ -69,9 +71,9 @@ def create_app():
 
 app = create_app()
 
-# Create DB tables automatically
-with app.app_context():
-    db.create_all()
+# # Create DB tables automatically
+# with app.app_context():
+
 
 
 if __name__ == '__main__':
